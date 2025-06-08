f0bd8c8d3833b369ea65b7ff8edff65d17d589c7
Ben Nelson <binelson13@gmail.com>
2025-06-07 19:07:24 -0500

    Adding deployment script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+docker build -t localhost/steps-ui:latest ./steps-ui --build-arg REACT_APP_API_URL=http://homelab.io/api
+docker build -t localhost/steps-api:latest ./Steps.Api
+docker save localhost/steps-ui:latest -o steps-ui.tar
+docker save localhost/steps-api:latest -o steps-api.tar
+k3s ctr images import steps-ui.tar 
+k3s ctr images import steps-api.tar 
+kubectl rollout restart deployment steps-ui
+kubectl rollout restart deployment steps-api 
