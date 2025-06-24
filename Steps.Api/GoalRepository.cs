090995977148b391b0fb6ac01d8e0893ee40e23c
Ben Nelson <binelson13@gmail.com>
2025-06-23 21:41:18 -0500

    Fixing build

--- Steps.Api/GoalRepository.cs
+++ Steps.Api/GoalRepository.cs
@@ -2,18 +2,27 @@ using Microsoft.Data.Sqlite;
 
 namespace Steps.Api;
 
-
+/// <summary>
+/// Repository for storing goal information in the database 
+/// </summary>
 public class GoalRepository {
     private readonly SqliteConnection _connection;
 
+    /// <summary>
+    /// Primary constructor 
+    /// </summary>
+    /// <param name="database"></param>
     public GoalRepository(Database database) {
         _connection = database.GetConnection();
     }
 
+    /// <summary>
+    /// Creates the sqlite table for goals if it doesn't already exist 
+    /// </summary>
     public void CreateTableIfNotExists() {
         SqliteCommand tableCmd = _connection.CreateCommand();
 
-        tableCmd.CommandText = 
+        tableCmd.CommandText =
         @"
         CREATE TABLE IF NOT EXISTS Goals (
             Id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -25,9 +34,13 @@ public class GoalRepository {
         _ = tableCmd.ExecuteNonQuery();
     }
 
+    /// <summary>
+    /// Add a goal entry to the database
+    /// </summary>
+    /// <param name="entry"></param>
     public void Add(GoalEntry entry) {
         SqliteCommand insertCmd = _connection.CreateCommand();
-        insertCmd.CommandText = 
+        insertCmd.CommandText =
         @"INSERT INTO Goals (
             GoalSteps
             GoalType)
@@ -38,7 +51,7 @@ public class GoalRepository {
         ";
 
         _ = insertCmd.Parameters.AddWithValue("$goalSteps", entry.GoalSteps);
-        _ = insertCmd.Parameters.AddWithValue("$goalType", entry.GoalType);
+        _ = insertCmd.Parameters.AddWithValue("$goalType", entry.Type);
 
         _ = insertCmd.ExecuteNonQuery();
     }
